bd7cee837fbecb93133953fcc91b056e06dc9ecf
FedePalmio <federico.palmiotto.stud@ispascalcomandini.it>
2024-10-21 08:23:22 +0200

    v0.1: creato progetto e base delle classi Libro e Biblioteca

---
+++ verificaLibreria/verificaLibreria/Biblioteca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificaLibreria
+{
+    internal class Biblioteca
+    {
+        string nome;
+        string indirizzo;
+        TimeOnly orarioApertura;
+        TimeOnly orarioChiusura;
+        List<Libro> libri;
+    }
+}
---
+++ verificaLibreria/verificaLibreria/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificaLibreria
+{
+    internal class Libro
+    {
+        string autore;
+        string libro;
+        DateTime annoPubblicazione;
+        string editore;
+        int numeroPagine;
+
+        public Libro() { }
+    }
+}
