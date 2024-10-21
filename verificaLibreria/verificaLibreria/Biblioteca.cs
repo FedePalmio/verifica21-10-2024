a2a299e0c8ddef45321adfeb479c02a9d0bb937c
FedePalmio <federico.palmiotto.stud@ispascalcomandini.it>
2024-10-21 08:44:21 +0200

    v0.2b: cominciato implementazione classe Biblioteca

--- verificaLibreria/verificaLibreria/Biblioteca.cs
+++ verificaLibreria/verificaLibreria/Biblioteca.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace verificaLibreria
 {
     internal class Biblioteca
     {
-        string nome;
-        string indirizzo;
-        TimeOnly orarioApertura;
-        TimeOnly orarioChiusura;
-        List<Libro> libri;
+        public string nome {  get; set; }
+        public string indirizzo { get; set; }
+        public TimeOnly orarioApertura { get; set; }
+        public TimeOnly orarioChiusura { get; set; }
+        public List<Libro> libri { get; }
+
+        public Biblioteca(string nome, string indirizzo, TimeOnly orarioApertura, TimeOnly orarioChiusura)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orarioApertura = orarioApertura;
+            this.orarioChiusura = orarioChiusura;
+            libri = new List<Libro>();
+        }
+
+        public void AggiungiLibro(string autore, string titolo, int annoPubblicazione, string editore, int numeroPagine)
+        {
+            libri.Add(new Libro(autore, titolo, annoPubblicazione, editore, numeroPagine));
+        }
+
+        public Libro Cerca(string titolo)
+        {
+            foreach (Libro libro in libri)
+            {
+                if(libro.titolo == titolo)
+                {
+                    return libro;
+                }
+            }
+
+            return null;
+        }
+
+
     }
 }
