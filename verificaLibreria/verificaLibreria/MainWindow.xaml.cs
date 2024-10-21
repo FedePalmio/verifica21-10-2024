e3d4021033a7a4643c793ebea75847715b4d46ff
FedePalmio <federico.palmiotto.stud@ispascalcomandini.it>
2024-10-21 09:18:29 +0200

    v0.2: iniziato implementazione wpf

--- verificaLibreria/verificaLibreria/MainWindow.xaml.cs
+++ verificaLibreria/verificaLibreria/MainWindow.xaml.cs
@@ -20,5 +20,83 @@ namespace verificaLibreria
         {
             InitializeComponent();
         }
+
+        Biblioteca biblioteca;
+
+        private void btnCreaBib_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblBibNome.Text == "Nome" || lblBibNome.Text == "")
+            {
+                MessageBox.Show("Il nome della biblioteca non è valido!");
+                return;
+            }
+            if (lblBibIndirizzo.Text == "Indirizzo" || lblBibIndirizzo.Text == "")
+            {
+                MessageBox.Show("L'indirizzo della biblioteca non è valido!");
+                return;
+            }
+            TimeOnly temp1;
+            if (!TimeOnly.TryParse(lblBibApertura.Text, out temp1))
+            {
+                MessageBox.Show("L'orario di apertura della biblioteca non è valido!");
+                return;
+            }
+            TimeOnly temp2;
+            if (!TimeOnly.TryParse(lblBibChiusura.Text, out temp2))
+            {
+                MessageBox.Show("L'orario di chiusura della biblioteca non è valido!");
+                return;
+            }
+
+            //biblioteca = new Biblioteca(lblBibNome.Text, lblBibIndirizzo.Text, lblBibApertura.Text, temp1, temp2);
+
+            lblBibNome.IsEnabled = false;
+            lblBibIndirizzo.IsEnabled = false;
+            lblBibApertura.IsEnabled = false;
+            lblBibChiusura.IsEnabled = false;
+            btnCreaBib.IsEnabled = false;
+
+            lblLibroPagine.IsEnabled = true;
+            lblLibroAnno.IsEnabled = true;
+            lblLibroAutore.IsEnabled = true;
+            lblLibroTitolo.IsEnabled = true;
+            lblLibroEditore.IsEnabled = true;
+            btnCreaLibro.IsEnabled = true;
+        }
+
+        private void btnCreaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblLibroAutore.Text == "Autore" || lblLibroAutore.Text == "")
+            {
+                MessageBox.Show("L'autore del libro non è valido!");
+                return;
+            }
+            if (lblLibroTitolo.Text == "Titolo" || lblLibroTitolo.Text == "")
+            {
+                MessageBox.Show("Il titolo del libro non è valido!");
+                return;
+            }
+            if (lblLibroEditore.Text == "Editore" || lblLibroEditore.Text == "")
+            {
+                MessageBox.Show("L'editore del libro non è valido!");
+                return;
+            }
+            int anno;
+            bool ok = int.TryParse(lblLibroAnno.Text, out anno);
+            if (!ok)
+            {
+                MessageBox.Show("L'anno di pubblicazione del libro non è valido!");
+                return;
+            }
+            int nPagine;
+            ok = int.TryParse(lblLibroPagine.Text, out nPagine);
+            if (!ok)
+            {
+                MessageBox.Show("Il numero di pagine del libro non è valido!");
+                return;
+            }
+
+            //biblioteca.AggiungiLibro(lblLibroAutore.Text, lblLibroTitolo.Text, anno, lblLibroEditore.Text, nPagine);
+        }
     }
 }
